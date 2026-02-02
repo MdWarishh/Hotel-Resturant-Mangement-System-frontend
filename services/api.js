71466244b3e89362f6dd214aa56ca38148f3fc02
MdWarishh <171806296+MdWarishh@users.noreply.github.com>
2026-02-02 23:20:08 +0530

    added url

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = typeof window !== 'undefined' && window.location.hostname !== 'localhost'
-  ? 'https://hotel-resturant-mangement-system.onrender.com/api' // Aapka Render URL
+  ? 'https://hotel-resturant-management-system-backend-production.up.railway.app/api' // Aapka Render URL
   : 'http://localhost:5000/api';
 
 const api = axios.create({
