4455bfeaae1a993c0eaf8393d4ca90a6e9e09991
MdWarishh <171806296+MdWarishh@users.noreply.github.com>
2026-02-14 02:26:18 +0530

    update pag

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,10 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
 
-  useEffect(() => {
-    router.push('/public');
-  }, [router]);
+   redirect('/public');
 
   return (
     <div className="min-h-screen flex items-center justify-center">
